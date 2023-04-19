a0530c751a32c13ab7458c9990b2dcb5f0507795
Ajoge Abdulmumin Adavize <ajogious@gmail.com>
2023-04-19 10:13:41 +0100

    updated game

--- README.md
+++
@@ -1,2 +0,0 @@
-#README.md
-this is a game application that can be play to guess a hidden number. if the guess is correct, the player wins
--- script.js
+++ script.js
@@ -1,91 +1,90 @@
-"use strict";
+'use strict';
+// html element
+const input = document.querySelector('.input-value');
+const btn = document.querySelector('.btn');
+const message = document.querySelector('.message');
+const gameLeft = document.querySelector('#game-left');
+const scores = document.querySelector('#score');
+const highestScore = document.querySelector('#highest-score');
 
-// === elements ===
-const hideQuestion = document.querySelector(".hide-question");
-const message = document.querySelector(".message");
-const inputNum = document.querySelector(".input-number");
-const guessBtn = document.querySelector(".btn");
-const gameLife = document.querySelector("#game-life");
-let score = document.querySelector("#score");
-const resetBtn = document.querySelector(".reset-btn");
-const highestScore = document.querySelector("#highest-score");
+function messageAlert(msg, addColors, removeColors) {
+  message.textContent = msg;
+  message.classList.add(addColors);
+  message.classList.remove(removeColors);
+}
 
-// let gameLives = 10;
-let scored = 10;
-let highestScores = 0;
+function gameLeftFunc(addColors, removeColors) {
+  gameLeft.classList.add(addColors);
+  gameLeft.classList.remove(removeColors);
+}
 
-// == time out for message ===
-const timeOut = () => {
-  message.style.display = "";
-  setTimeout(() => {
-    message.style.display = "none";
-  }, 1500);
-};
+function output() {
+  let randomNum = Math.trunc(Math.random() * 20 + 1);
+  let gameLife = 10;
+  let score = 10;
 
-// === button disabled func ===
-const disabledFunc = () => {
-  guessBtn.disabled = true;
-  inputNum.disabled = true;
-};
+  gameLeft.textContent = gameLife;
+  scores.textContent = 0;
+  highestScore.textContent = 0;
+  messageAlert('Guess number from 1 to 20');
 
-// === random number ===
-let roundNum = Math.trunc(Math.random() * 20) + 1;
-console.log(roundNum);
-// === btn on click ===
-guessBtn.addEventListener("click", function () {
-  let val = Number(inputNum.value);
-  // === if player input nothing ===
-  if (!inputNum.value) {
-    message.textContent = `Input a number from 1 to 20`;
-    hideQuestion.textContent = "?";
-    timeOut();
-    // === if correct guess === //
-  } else if (val === roundNum) {
-    hideQuestion.textContent = roundNum;
-    document.querySelector(".game-over").textContent = `Correct Guess`;
-    document.querySelector(".container").style.backgroundColor = "#333";
-    disabledFunc();
-    if (scored > highestScores) {
-      highestScores = scored;
-      score.textContent = highestScores;
-      highestScore.textContent = highestScores;
-    } else {
-      score.textContent = gameLife.textContent;
+  btn.addEventListener('click', (e) => {
+    let inputVal = Number(input.value);
+
+    if (btn.value === 'Guess') {
+      if (input.value === '' || input.value < 1 || input.value > 20) {
+        messageAlert('Input a number from 1 to 20 only');
+      } else if (inputVal === randomNum) {
+        // if correct guess?
+        messageAlert(`Correct guess: ${randomNum}`, 'success', 'warning');
+        input.disabled = true;
+        btn.value = 'Restart';
+
+        scores.textContent = gameLeft.textContent;
+
+        if (score > highestScore.textContent) {
+          highestScore.textContent = score;
+        }
+      } else {
+        inputVal > randomNum
+          ? messageAlert('Guess to high', 'warning', 'success')
+          : messageAlert('Guess to low', 'warning', 'success');
+        gameLife--;
+        score = gameLife;
+        gameLeft.textContent = score;
+
+        if (gameLife < 5) {
+          gameLeftFunc('warning');
+        }
+
+        if (gameLife === 0) {
+          messageAlert(`Game over. The number was ${randomNum}`, 'danger');
+          gameLeftFunc('danger');
+          input.disabled = true;
+          btn.value = 'Restart';
+        }
+      }
+    } else if (btn.value === 'Restart') {
+      randomNum = Math.trunc(Math.random() * 20 + 1);
+      gameLife = 10;
+      score = 0;
+
+      gameLeft.textContent = gameLife;
+      scores.textContent = score;
+      input.disabled = false;
+      btn.value = 'Guess';
+      score = gameLife;
+
+      gameLeft.textContent = score;
+      messageAlert('Guess number from 1 to 20', undefined, 'danger');
+      message.classList.remove('success');
+      message.classList.remove('warning');
+      gameLeftFunc(undefined, 'danger');
+      gameLeft.classList.remove('warning');
     }
-  }
 
-  // === if incorrect number inputed ===
-  else if (inputNum.value !== hideQuestion.textContent) {
-    message.textContent = `Incorrect Guess, try again`;
-    message.style.color = "#e70f0f";
-    hideQuestion.textContent = "?";
-    scored--;
-    gameLife.textContent = scored;
-    timeOut();
-
-    //   // === game over if score less than 1 ===
-    if (gameLife.textContent < 1) {
-      document.querySelector(".container").style.backgroundColor = "#f00";
-      document.querySelector(
-        ".game-over"
-      ).textContent = `Game Over. The correct number was ${roundNum}`;
-      disabledFunc();
-    }
-  }
-});
-
-// === reset button ===
-resetBtn.addEventListener("click", () => {
-  scored = 10;
-  roundNum = Math.trunc(Math.random() * 20) + 1;
-  console.log(roundNum);
-  hideQuestion.textContent = "?";
-  message.style.color = "";
-  message.textContent = "";
-  inputNum.value = "";
-  gameLife.textContent = scored;
-  document.querySelector(".game-over").textContent = "";
-  document.querySelector(".container").style.backgroundColor = "#fff";
-  guessBtn.disabled = false;
-  inputNum.disabled = false;
-});
+    input.value = '';
+    e.preventDefault();
+  });
+}
+output();
